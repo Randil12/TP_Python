307b8d07ac17afef6cb103c087a4eee3c4f373a2
Randil <nguyen_david2003@outlook.fr>
2024-05-24 15:38:09 +0200

    Error from get_all_teams , create_team is good , add some source to ReadMe.md

--- README.md
+++ README.md
@@ -3,3 +3,28 @@ TP python avancée
 
 
 Pour lancer le programme veuillez suivre les étapes suivantes : 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Source : 
+Exercice Docker
+https://stackoverflow.com/questions/2633218/how-can-i-select-all-rows-with-sqlalchemy
+https://thepythoncode.com/article/building-crud-app-with-flask-and-sqlalchemy
+https://stackoverflow.com/questions/73961938/flask-sqlalchemy-db-create-all-raises-runtimeerror-working-outside-of-applicat
+https://stackoverflow.com/questions/18807322/sqlalchemy-foreign-key-relationship-attributes
--- app.py
+++ app.py
@@ -1,14 +1,44 @@
 from flask import Flask ,request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import OperationalError
+
+
 app = Flask(__name__)
+#Config de SQLAlchymy 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@localhost/TP'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+#ça c'est pour créer une table Team
+class Team(db.Model):
+    team_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60) , nullable=False)
+    
+
+class Player(db.Model):
+    player_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('team.team_id'), nullable=False)
+
+#Retourne toutes les teams 
 @app.route('/get_all_teams' , methods=['GET'])
-def test():
-    return 'Hello, World!'
+def get_teams():
+    AllTeams = Team.query.all()
+    return jsonify([{'id': team.id, 'name': team.name} for team in AllTeams])
 
-
+#Créé une team
 @app.route('/create_team' , methods=['POST'])
-def test2():
-    return request.get_json()
+def create_team():
+    # DataTeam = request.get_json()
+    # DataTeam2 = request.json.get('name')
+    
+    team = Team(name=request.json.get('name'))
+    db.session.add(team)
+    db.session.commit()
+    return jsonify({'id': team.team_id})
+
+
+with app.app_context():
+    db.create_all()
+
 app.run()
